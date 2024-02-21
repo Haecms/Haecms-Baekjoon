586729b7561d380a9297b0f3de460cd7a5e288b3
Haecms <137016400+Haecms@users.noreply.github.com>
2024-02-21 23:42:41 +0900

    [level 0] Title: 배열 조각하기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.cpp
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 vector<int> solution(vector<int> arr, vector<int> query) {
-    	for (int i = 0; i < query.size(); i++) {
+    for (int i = 0; i < query.size(); i++) {
 		if (i % 2 == 0) {
 			arr.erase(arr.begin() + query[i]+1, arr.end());
 		}
