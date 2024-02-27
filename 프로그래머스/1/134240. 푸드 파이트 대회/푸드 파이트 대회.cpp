b67f026598f0540243a4c5000b1af65c9a730776
Haecms <137016400+Haecms@users.noreply.github.com>
2024-02-27 21:44:33 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.02 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -4,18 +4,14 @@
 using namespace std;
 
 string solution(vector<int> food) {
-    vector<int> hello = { 0 };
+    string answer = "0";
     for (int i = food.size()-1; i >= 1; i--) {
-
 	    for (int j = 0; j < food[i] / 2; j++) {
-		    hello.push_back(i);
-		    hello.insert(hello.begin(), i);
+		    answer = to_string(i) + answer + to_string(i);
 	    }
     }
-    string answer = "";
-    for (int i = 0; i < hello.size(); i++) {
-	    answer += to_string(hello[i]);
-    }
+    
+
     
     return answer;
 }
