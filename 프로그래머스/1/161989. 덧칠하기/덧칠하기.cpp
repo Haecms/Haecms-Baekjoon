93f587d301466557d80c1465605e2c39f58fd1df
Haecms <137016400+Haecms@users.noreply.github.com>
2024-03-01 23:47:19 +0900

    [level 1] Title: 덧칠하기, Time: 0.18 ms, Memory: 4.99 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
@@ -5,28 +5,16 @@
 using namespace std;
 
 int solution(int n, int m, vector<int> section) {
-	unordered_map<int, int> section2;
-	int count = 0;
+	int area = 0;
 	int answer = 0;
-
-	for (int i = 1; i <= n; i++) {
-		if (i == section[count]) {
-			count++;
-			section2[i] = 1;
+	for (int i = 0; i < section.size(); i++) {
+		if (area < section[i]) {
+			answer++;
+			area = section[i] + m - 1;
 		}
 		else {
-			section2[i] = 0;
+			continue;
 		}
-	}
-	count = 1;
-	while (count <= n) {
-		if (section2[count]) {
-			for (int i = count; i < count + m; i++) {
-				section2[i] =0;
-			}
-			answer++;
-		}
-		count++;
 	}
     return answer;
 }
