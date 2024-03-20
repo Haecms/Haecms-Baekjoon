6410e4737f2ddfba1ca8bc5e132401aaa2ee6399
Haecms <137016400+Haecms@users.noreply.github.com>
2024-03-20 23:54:07 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 0.05 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor) {
+    vector<int> answer;
+
+	for (int i = 0; i < arr.size(); i++) {
+		if (arr[i] % divisor == 0) {
+			answer.push_back(arr[i]);
+		}
+	}
+	for (int i = 0; i < answer.size(); i++) {
+		for (int j = i + 1; j < answer.size(); j++) {
+			if (answer[i] > answer[j]) {
+				int temp = answer[i];
+				answer[i] = answer[j];
+				answer[j] = temp;
+			}
+		}
+	}
+	if (answer.size() == 0) answer.push_back(-1);
+    return answer;
+}
