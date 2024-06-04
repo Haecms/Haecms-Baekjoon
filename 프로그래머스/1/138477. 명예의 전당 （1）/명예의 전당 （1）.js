a38f18f8d8a76764dba44cc5340e3457f82b101d
Haecms <137016400+Haecms@users.noreply.github.com>
2024-06-04 22:07:48 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -0,0 +1,14 @@
+function solution(k, score) {
+    var answer = [];
+    let honor = [];
+    
+    for (let i = 0; i < score.length; i++) {
+      honor.push(score[i]);
+      honor.sort((a, b) => b - a);
+      if (honor.length > k) {
+        honor.pop();
+      }
+      answer.push(honor[honor.length - 1]);
+    }
+    return answer;
+}
