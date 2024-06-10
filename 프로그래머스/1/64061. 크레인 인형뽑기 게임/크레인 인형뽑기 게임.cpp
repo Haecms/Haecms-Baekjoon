ba598d46964251126c67e8b9fe2ad833881cf587
Haecms <137016400+Haecms@users.noreply.github.com>
2024-06-10 23:56:06 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.03 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
@@ -11,18 +11,16 @@ int solution(vector<vector<int>> board, vector<int> moves) {
         int x = moves[i] - 1;
         for (int y = 0; y < board.size(); y++) {
             if (board[y][x] != 0) {
-                store.push_back(board[y][x]);
+                if (store.size() > 0 && store[store.size() - 1] == board[y][x]) {
+                    store.erase(store.begin() + (store.size() - 1));
+                    answer += 2;
+                }
+                else {
+                    store.push_back(board[y][x]);
+                }
                 board[y][x] = 0;
                 break;
             }
-        }
-    }
-    for (int i = 1; i < store.size(); i++) {
-        if ( i> 0 && store[i] == store[i - 1]) {
-            answer += 2;
-            store.erase(store.begin() + i-1);
-            store.erase(store.begin() + i-1);
-            i-=2;
         }
     }
     return answer;
