90231d3c8fc9b66d5de41ebb501f6f304edce9cc
Haecms <137016400+Haecms@users.noreply.github.com>
2024-07-14 21:37:37 +0900

    [Silver III] Title: 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.cc
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int a;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+	while (scanf("%d", &a) != EOF) {
+        ll b = 0, c = 0;
+		do {
+			b = b * 10 + 1;
+			b %= a;
+			c++;
+		} while (b % a);
+		cout << c << "\n";
+	}
+}
