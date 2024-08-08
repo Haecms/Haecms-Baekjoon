f6559b220ab556c06138a8daed317367904bac38
Haecms <137016400+Haecms@users.noreply.github.com>
2024-08-08 23:09:27 +0900

    [Silver I] Title: 안전 영역, Time: 24 ms, Memory: 2428 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int max_n = 101;
+int N, ny, nx, ret = 0;
+int a[max_n][max_n], visited[max_n][max_n];
+int dy[4] = { -1, 0, 1, 0 };
+int dx[4] = { 0, -1, 0, 1 };
+
+void dfs(int y, int x, int k) {
+	visited[y][x] = 1;
+	for (int i = 0; i < 4; i++) {
+		ny = y + dy[i];
+		nx = x + dx[i];
+		if (ny < 0 || ny >= N || nx < 0 || nx >= N) continue;
+		if (a[ny][nx] <= k || visited[ny][nx]) continue;
+		dfs(ny, nx, k);
+	}
+}
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> a[i][j];
+		}
+	}
+	for (int k = 1; k < 101; k++) {
+		int count = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (a[i][j] > k && !visited[i][j]) {
+					dfs(i, j, k);
+					count++;
+				}
+			}
+		}
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				visited[i][j] = 0;
+			}
+		}
+		if (count > ret) ret = count;
+	}
+	if (!ret) ret = 1;
+	cout << ret;
+}
