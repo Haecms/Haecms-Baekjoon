b49b1949b165e6a4b9c250b949107e9185ebed5c
Haecms <137016400+Haecms@users.noreply.github.com>
2024-08-12 22:57:45 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+string s;
+map<char, int> moum = {
+	{'a', 1}, {'b', 0}, {'c', 0}, {'d', 0},	{'e', 1},
+	{'f', 0}, {'g', 0}, {'h', 0}, {'i', 1}, {'j', 0},
+	{'k', 0}, {'l', 0}, {'m', 0}, {'n', 0}, {'o', 1},
+	{'p', 0}, {'q', 0}, {'r', 0}, {'s', 0}, {'t', 0},
+	{'u', 1}, {'v', 0}, {'w', 0}, {'x', 0}, {'y', 0},
+	{'z', 0}
+};
+int main()
+{
+	while (true) {
+		cin >> s;
+		if (s == "end") break;
+		bool flag1 = false;
+		bool flag2 = true;
+		for (char a : s) {
+			if (moum[a]) {
+				flag1 = true;
+				break;
+			}
+		}
+		stack<char> munjayeol;
+		for (char a : s) {
+			if (munjayeol.size() == 0) { 
+				munjayeol.push(a);
+				continue; 
+			}
+			if (munjayeol.size() == 1) {
+				if (munjayeol.top() == a && a != 'e' && a != 'o') {
+					flag2 = false;
+					break;
+				}
+				else {
+					munjayeol.push(a);
+					continue;
+				}
+			}
+			if (munjayeol.top() == a) {
+				if (a != 'e' && a != 'o') {
+					flag2 = false;
+					break;
+				}
+			}
+			if (moum[munjayeol.top()] == moum[a]) {
+				char n = munjayeol.top();
+				munjayeol.pop();
+				if (moum[munjayeol.top()] == moum[a]) {
+					flag2 = false;
+					break;
+				}
+				else {
+					munjayeol.push(n);
+				}
+			}
+			munjayeol.push(a);
+		}
+		if (!flag1 || !flag2) {
+			cout << "<" << s << ">" << " is not acceptable.\n";
+		}
+		else {
+			cout << "<" << s << ">" << " is acceptable.\n";
+		}
+	}
+}
