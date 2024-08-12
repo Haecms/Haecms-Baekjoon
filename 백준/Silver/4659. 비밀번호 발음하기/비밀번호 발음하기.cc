b1023db4da7b5db4eeb025c47dcee8b16241186e
Haecms <137016400+Haecms@users.noreply.github.com>
2024-08-12 23:22:37 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
@@ -17,51 +17,27 @@ int main()
 		bool flag1 = false;
 		bool flag2 = true;
 		for (char a : s) {
-			if (moum[a]) {
-				flag1 = true;
-				break;
-			}
+			if (moum[a]) { flag1 = true; break; }
 		}
 		stack<char> munjayeol;
 		for (char a : s) {
-			if (munjayeol.size() == 0) { 
-				munjayeol.push(a);
-				continue; 
-			}
-			if (munjayeol.size() == 1) {
-				if (munjayeol.top() == a && a != 'e' && a != 'o') {
-					flag2 = false;
-					break;
+			if (munjayeol.size() != 0) 
+			{
+				if (munjayeol.top() == a) {
+					if (a != 'e' && a != 'o') { flag2 = false; break; }
 				}
-				else {
-					munjayeol.push(a);
-					continue;
-				}
-			}
-			if (munjayeol.top() == a) {
-				if (a != 'e' && a != 'o') {
-					flag2 = false;
-					break;
-				}
-			}
-			if (moum[munjayeol.top()] == moum[a]) {
-				char n = munjayeol.top();
-				munjayeol.pop();
-				if (moum[munjayeol.top()] == moum[a]) {
-					flag2 = false;
-					break;
-				}
-				else {
-					munjayeol.push(n);
+				if (munjayeol.size() > 1) {
+					if (moum[munjayeol.top()] == moum[a]) {
+						char n = munjayeol.top();
+						munjayeol.pop();
+						if (moum[munjayeol.top()] == moum[a]) { flag2 = false; break; }
+						else { munjayeol.push(n); }
+					}
 				}
 			}
 			munjayeol.push(a);
 		}
-		if (!flag1 || !flag2) {
-			cout << "<" << s << ">" << " is not acceptable.\n";
-		}
-		else {
-			cout << "<" << s << ">" << " is acceptable.\n";
-		}
+		if (!flag1 || !flag2) { cout << "<" << s << ">" << " is not acceptable.\n"; }
+		else { cout << "<" << s << ">" << " is acceptable.\n"; }
 	}
 }
