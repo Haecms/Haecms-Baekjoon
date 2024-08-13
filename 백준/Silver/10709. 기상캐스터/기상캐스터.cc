3a2b08746ab6b184e699c5c04600c107484b3d5a
Haecms <137016400+Haecms@users.noreply.github.com>
2024-08-13 21:32:32 +0900

    [Silver V] Title: 기상캐스터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.cc
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.cc
@@ -1,24 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 int n, m;
-queue<int> visited;
 char c;
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 	cin >> n >> m;
 	for (int i = 0; i < n; i++) {
 		int cnt = -1;
 		for (int j = 0; j < m; j++) {
 			cin >> c;
-			if (c == 'c') { visited.push(0); cnt = 0; }
-			else if(cnt >= 0) { cnt++; visited.push(cnt); }
-			else { visited.push(cnt); }
-		}
-	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cout << visited.front() << " ";
-			visited.pop();
+			if (c == 'c') { cout << "0" << " "; cnt = 0; }
+			else if (cnt >= 0) { cnt++;  cout << cnt << " "; }
+			else { cout << cnt << " "; }
 		}
 		cout << '\n';
 	}
