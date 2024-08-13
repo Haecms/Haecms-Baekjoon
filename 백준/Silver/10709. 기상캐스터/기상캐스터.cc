0af8da3e895517a23235284c7c764fc0f9d3b082
Haecms <137016400+Haecms@users.noreply.github.com>
2024-08-13 21:17:57 +0900

    [Silver V] Title: 기상캐스터, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.cc
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.cc
@@ -4,24 +4,16 @@ const int mx=104;
 int n, m;
 int visited[mx][mx];
 char c;
-void goRight(int y, int x, int k) {
-	if (visited[y][x] == 0 || x >= m) return;
-	visited[y][x] = k;
-	goRight(y, x + 1, k + 1);
-}
 int main()
 {
 	cin >> n >> m;
 	for (int i = 0; i < n; i++) {
+		int cnt = -1;
 		for (int j = 0; j < m; j++) {
 			cin >> c;
-			if(c == 'c') visited[i][j] = 0;
-			else visited[i][j] = -1;
-		}
-	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if(visited[i][j] == 0 && i < n && j+1 < m) goRight(i, j + 1, 1);
+			if (c == 'c') { visited[i][j] = 0; cnt = 0; }
+			else if(cnt >= 0) { cnt++; visited[i][j] = cnt; }
+			else { visited[i][j] = cnt; }
 		}
 	}
 	for (int i = 0; i < n; i++) {
