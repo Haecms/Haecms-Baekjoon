dc656e1664282958cf15a5d0cc4e830baea03c64
Haecms <137016400+Haecms@users.noreply.github.com>
2024-08-14 23:54:48 +0900

    [Silver III] Title: 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.cc
+++ 백준/Silver/4375. 1/1.cc
@@ -5,8 +5,8 @@ int main()
 {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	while (scanf("%d", &a) != EOF) {
-		int c=1, m = 1;
-		while (m % a) { m = m * 10 + 1; m %= a; c++; }
+		int c=1, m = 0;
+		while (m = (m*10+1)%a ) c++;
 		cout << c << '\n';
 	}
 }
