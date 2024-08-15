68523fea8394ac59901ff64dc05cb106bc056297
Haecms <137016400+Haecms@users.noreply.github.com>
2024-08-15 13:06:42 +0900

    [Silver III] Title: NBA 농구, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2852. NBA 농구/NBA 농구.cc
+++ 백준/Silver/2852. NBA 농구/NBA 농구.cc
@@ -10,32 +10,16 @@ int main() {
         cin >> m >> s;
         m == 1 ? A++ : B++;
         int sec = (stoi(string(1, s[0]) + string(1, s[1])) * 60) + stoi(string(1, s[3]) + string(1, s[4]));
-        if (A > B && wA < 0) { 
-            wA = sec;
-        }
-        else if (A < B && wB < 0) { 
-            wB = sec; 
-        }
+        if (A > B && wA < 0) { wA = sec; }
+        else if (A < B && wB < 0) { wB = sec; }
         else if(A == B){
-            if (m == 1) {
-                dB += sec - wB;
-                wB = -1; 
-            }
-            else {
-                dA += sec - wA;
-                wA = -1; 
-            }
+            if (m == 1) { dB += sec - wB; wB = -1; }
+            else { dA += sec - wA; wA = -1; }
         }
     }
-    if (A == B) {
-        printf("%02d:%02d\n", dA / 60, dA % 60);
-        printf("%02d:%02d\n", dB / 60, dB % 60);
-    }
-    else {
-        if (wA > 0) dA += 2880 - wA;
-        else dB += 2880 - wB;
-        printf("%02d:%02d\n", dA / 60, dA % 60);
-        printf("%02d:%02d\n", dB / 60, dB % 60);
-    }
+    if(A > B)  dA += 2880 - wA;
+    else if(A < B)  dB += 2880 - wB;
+    printf("%02d:%02d\n", dA / 60, dA % 60);
+    printf("%02d:%02d\n", dB / 60, dB % 60);
     return 0;
 }
