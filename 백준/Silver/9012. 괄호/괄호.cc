8b6d6d2ee16d2ad3cf6f4a267a3a58a8ba0461e7
Haecms <137016400+Haecms@users.noreply.github.com>
2024-08-16 23:58:01 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.cc
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -5,14 +5,11 @@ string s;
 int main() {
 	cin >> n;
 	while (n--) {
-		cin >> s;
-		int l = 0;
-		for (int i = 0; i < s.size(); i++) {
-			if (s[i] == '(') l++;
-			else l--;
-			if (l < 0) { cout << "NO" << '\n'; l = 100; break; }
+		cin >> s; int l = 0;
+		for (char a : s) {
+			a == '(' ? l++ : l--;
+			if (l < 0) { cout << "NO"; l = 100; break; }
 		}
-		if (l == 0) cout << "YES" << '\n';
-		else if (l != 100) cout << "NO" << '\n';
+		l == 0 ? cout << "YES\n" : l != 100 ? cout << "NO\n" : cout << '\n';
 	}
 }
