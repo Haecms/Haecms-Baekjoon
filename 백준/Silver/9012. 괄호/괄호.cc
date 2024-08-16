e3f8fa5389da89ce5a481a9dd81d5990c33feb3c
Haecms <137016400+Haecms@users.noreply.github.com>
2024-08-17 00:28:52 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.cc
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -1,15 +1,16 @@
-#include<iostream>
-using namespace std;
-int n;
-string s;
+#include <stdio.h>
+#pragma warning(disable:4996)
 int main() {
-	cin >> n;
+	int n;
+	scanf("%d", &n);
 	while (n--) {
-		cin >> s; int l = 0;
+		char s[51];
+		scanf("%s", s); int l = 0, r = 0;
 		for (char a : s) {
+			if (a == '\0') break;
 			a == '(' ? l++ : l--;
-			if (l < 0) { cout << "NO"; l = 100; break; }
+			if (l < 0) { r++; break; }
 		}
-		l == 0 ? cout << "YES\n" : l != 100 ? cout << "NO\n" : cout << '\n';
+		l != 0 || r != 0 ? printf("NO\n") : printf("YES\n");
 	}
 }
