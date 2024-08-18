37cc63b86b7256939978b957ea2583215f8e1bda
Haecms <137016400+Haecms@users.noreply.github.com>
2024-08-18 19:18:55 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -4,21 +4,24 @@
 using namespace std;
 string s;
 int main() {
-    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 	while (true) {
 		stack<char> c;
 		getline(cin, s);
 		if (s == ".") break;
 		for (char a : s) {
-			if (a != '(' && a != ')' && a != '[' && a != ']') continue;
-			if (c.size() == 0 && (a == ']' || a == ')')) { c.push(a); break; }
-			if (a == '(' || a == '[') { c.push(a); continue; }
-			else {
-				if (c.top() == '(' && a == ')') c.pop();
-				else if (c.top() == '[' && a == ']') c.pop();
-				else break; 
+			if (a == '(' || a == '[') c.push(a);
+			else if (a == ')')
+			{
+				if (c.empty() || c.top() == '[') { c.push(a); break; }
+				if (!c.empty() || c.top() == '(') c.pop();
+			}
+			else if (a == ']')
+			{
+				if (c.empty() || c.top() == '(') { c.push(a); break; }
+				if (!c.empty() || c.top() == '[') c.pop();
 			}
 		}
-		c.size() == 0 ? cout<<"yes\n" : cout << "no\n";
+		c.size() == 0 ? cout << "yes\n" : cout << "no\n";
 	}
 }
