75c6f4f1518edb8eab4f3b4ab28892613f80e7fa
Haecms <137016400+Haecms@users.noreply.github.com>
2024-09-05 22:26:42 +0900

    [Bronze IV] Title: 헛간 청약, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/19698. 헛간 청약/헛간 청약.cc
+++ 백준/Bronze/19698. 헛간 청약/헛간 청약.cc
@@ -1,7 +1,6 @@
 #include<stdio.h>
 int main() {
 	int N,W,H,L;
-	scanf("%d", &N); scanf("%d", &W); scanf("%d", &H); scanf("%d", &L);
-	N = (W / L) * (H / L) > N ? N : (W / L) * (H / L);
-	printf("%d", N);
+	scanf("%d %d %d %d", &N, &W, &H, &L);
+	printf("%d", (W/L)*(H/L) > N ? N : (W/L)*(H/L));
 }
