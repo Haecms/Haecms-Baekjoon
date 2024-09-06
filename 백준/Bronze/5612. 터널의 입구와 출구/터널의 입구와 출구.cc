ad2ad66fafed05fbcfb7155b5e149277758108e7
Haecms <137016400+Haecms@users.noreply.github.com>
2024-09-06 20:48:44 +0900

    [Bronze III] Title: 터널의 입구와 출구, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/5612. 터널의 입구와 출구/터널의 입구와 출구.cc
+++ 백준/Bronze/5612. 터널의 입구와 출구/터널의 입구와 출구.cc
@@ -1,13 +1,2 @@
 #include<cstdio>
-int main() {
-	int n, m, N,M, r;
-	scanf("%d %d", &n, &m);
-	int s = r = m;
-	while (n--) {
-		scanf("%d %d", &N, &M);
-		s += N - M;
-		r = s > r ? s : r;
-		if (s < 0) break;
-	}
-	s < 0 ? printf("%d", 0) : printf("%d", r);
-}
+int main(){int n,m,N,M,r;scanf("%d %d",&n,&m);int s=r=m;while(n--){scanf("%d %d",&N,&M);s+=N-M;r=s>r?s:r;if(s<0)break;}s<0?printf("%d",0):printf("%d",r);}
