15499afd7f072e6235b42a1c00b8ad22995374f4
Haecms <137016400+Haecms@users.noreply.github.com>
2024-09-07 01:07:11 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int N, d, ret=0,time=0, a[1001];
+int main() 	{
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++) scanf("%d", &a[i]);
+	for (int i = 0; i < N; i++) {
+		for (int j = i + 1; j < N; j++) {
+			if (a[i] > a[j]) {
+				int temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+	for (int i = 0; i < N; i++) {
+		time = a[i] + time;
+		ret += time;
+	}
+	printf("%d", ret);
+	return 0;
+}
