d4cd9b09cbe77fe394d0d2ffae6f90df0a55a56e
Haecms <137016400+Haecms@users.noreply.github.com>
2024-09-08 01:47:40 +0900

    [Silver IV] Title: 도로와 신호등, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/2980. 도로와 신호등/도로와 신호등.cc
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+	int n, m, d, r, g, time = 0, prevd = 0;
+	scanf("%d%d", &n, &m);
+	
+	for (int i = 0; i < n; i++) {
+		scanf("%d%d%d", &d, &r, &g);
+		time += d - prevd;
+		prevd = d;
+		time += time%(r+g) >= r ? 0 : r - time%(r+g);
+	}
+	printf("%d", time + (m-prevd));
+	return 0;
+}
