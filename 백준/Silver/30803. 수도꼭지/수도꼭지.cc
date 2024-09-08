94e529b559c2a7050fce9c0fd42d3fb09d4a5df7
Haecms <137016400+Haecms@users.noreply.github.com>
2024-09-08 11:58:07 +0900

    [Silver IV] Title: 수도꼭지, Time: 92 ms, Memory: 3584 KB -BaekjoonHub

--- 백준/Silver/30803. 수도꼭지/수도꼭지.cc
+++ 백준/Silver/30803. 수도꼭지/수도꼭지.cc
@@ -1,16 +1,16 @@
-#include<stdio.h>
+#include<iostream>
+using namespace std;
 int c[200002], g[200002];
 int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	long long a, b, ret=0, d, e, f;
-	scanf("%lld", &a);
-	for (long long i = 1; i <= a; i++) {
-		scanf("%lld", &c[i]);
-		ret += c[i]; g[i] = 0;
-	}
-	scanf("%lld", &b); printf("%lld\n", ret);
+	cin >> a;
+	for (long long i = 1; i <= a; i++) { cin >> c[i]; g[i] = 0; ret += c[i]; }
+	cin >> b;
+	cout << ret << '\n';
 	while (b--) {
-		scanf("%lld", &d);
-		d == 1 ? scanf("%lld%lld", &e, &f) : scanf("%lld", &e);
+		cin >> d;
+		d == 1 ? cin >> e >> f : cin >> e;
 		if (d == 1) {
 			if(!g[e]) ret += f-c[e];
 			c[e] = f;
@@ -19,9 +19,8 @@ int main() {
 			if (g[e]) ret += c[e];
 			else ret -= c[e];
 			g[e] = g[e] ? 0 : 1;
-			
 		}
-		printf("%lld\n", ret);
+		cout << ret << '\n';
 	}
 	return 0;
 }
