16f763cbb97ba56b74fc37b27c0704c9995c9792
Haecms <137016400+Haecms@users.noreply.github.com>
2024-09-08 12:13:13 +0900

    [Silver IV] Title: 수도꼭지, Time: 84 ms, Memory: 3584 KB -BaekjoonHub

--- 백준/Silver/30803. 수도꼭지/수도꼭지.cc
+++ 백준/Silver/30803. 수도꼭지/수도꼭지.cc
@@ -5,20 +5,19 @@ int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	long long a, b, ret=0, d, e, f;
 	cin >> a;
-	for (long long i = 1; i <= a; i++) { cin >> c[i]; g[i] = 0; ret += c[i]; }
-	cin >> b;
+	for (long long i = 1; i <= a; i++) { cin >> c[i]; ret += c[i]; g[i] = 0; }
 	cout << ret << '\n';
+	cin >> b;
 	while (b--) {
-		cin >> d;
-		d == 1 ? cin >> e >> f : cin >> e;
+		cin >> d >> e;
 		if (d == 1) {
+			cin >> f;
 			if(!g[e]) ret += f-c[e];
 			c[e] = f;
 		}
 		else {
-			if (g[e]) ret += c[e];
-			else ret -= c[e];
-			g[e] = g[e] ? 0 : 1;
+			g[e] ^= 1;
+			ret += g[e] ? -c[e] : c[e];
 		}
 		cout << ret << '\n';
 	}
