5999399a026564b589e9d76e8944f0832500f691
Haecms <137016400+Haecms@users.noreply.github.com>
2024-09-11 23:55:27 +0900

    [Gold V] Title: 보물섬, Time: 76 ms, Memory: 2052 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.cc
+++ 백준/Gold/2589. 보물섬/보물섬.cc
@@ -1,22 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, m, visited[51][51], mx=0;
+int n, m, visited[51][51], mx = 0,z[2501], t[2501], P,T;
 char a[51][51];
 int dy[4] = { -1, 0, 1, 0 };
 int dx[4] = { 0, 1, 0, -1 };
 void bfs(int y, int x) {
 	memset(visited, 0, sizeof(visited));
 	visited[y][x] = 1;
-	queue<pair<int, int>> q;
-	q.push({ y, x});
-	while (q.size()) {
-		tie(y, x) = q.front(), q.pop();
+	z[0] = y; t[0] = x; P = 0; T = 0;
+	int count = 1;
+	while (count--) {
+		int cy = z[P];
+		int cx = t[P++];
 		for (int i = 0; i < 4; i++) {
-			int ny = dy[i] + y;
-			int nx = dx[i] + x;
+			int ny = dy[i] + cy;
+			int nx = dx[i] + cx;
 			if (ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx] || a[ny][nx] == 'W') continue;
-			visited[ny][nx] = visited[y][x] + 1;
-			q.push({ ny, nx });
+			visited[ny][nx] = visited[cy][cx] + 1;
+			count++;
+			z[++T] = ny;
+			t[T] = nx;
 			mx = max(mx, visited[ny][nx]);
 		}
 	}
