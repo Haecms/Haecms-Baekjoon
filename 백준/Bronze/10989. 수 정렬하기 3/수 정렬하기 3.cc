000f6aa3b7e14ad533a85db4b1212b103bb894b9
Haecms <137016400+Haecms@users.noreply.github.com>
2024-09-12 00:19:42 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1476 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -1,15 +1,17 @@
-#include<stdio.h>
+#include<iostream>
+using namespace std;
 int a, b[10001], c;
 int main() {
-	scanf("%d", &a);
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> a;
 	for (int i = 0; i < a; i++) {
-		scanf("%d", &c);
+		cin >> c;
 		b[c] += 1;
 	}
 	
 	for (int i = 0; i < 10001; i++) {
 		while (b[i]) {
-			printf("%d\n", i);
+			cout << i << '\n';
 			b[i]--;
 		}
 	}
