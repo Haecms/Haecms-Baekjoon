c74009c61870ba0d6ce3fc41df1adbb1b459ebf5
Haecms <137016400+Haecms@users.noreply.github.com>
2024-09-11 23:10:07 +0900

    [Gold V] Title: 보물섬, Time: 136 ms, Memory: 1240 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.cc
+++ 백준/Gold/2589. 보물섬/보물섬.cc
@@ -1,41 +1,39 @@
 #include<stdio.h>
-#include<vector>
 #include<queue>
 #include<tuple>
 #include<memory.h>
 using namespace std;
-int n, m, visited[51][51], y, x, mx=0;
+int n, m, visited[51][51], mx=0;
 char a[51][51];
-vector<pair<int, int>> v;
 int dy[4] = { -1, 0, 1, 0 };
 int dx[4] = { 0, 1, 0, -1 };
+void bfs(int y, int x) {
+	memset(visited, 0, sizeof(visited));
+	visited[y][x] = 1;
+	queue<pair<int, int>> q;
+	q.push({ y, x});
+	while (q.size()) {
+		tie(y, x) = q.front(), q.pop();
+		for (int i = 0; i < 4; i++) {
+			int ny = dy[i] + y;
+			int nx = dx[i] + x;
+			if (ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx] || a[ny][nx] == 'W') continue;
+			visited[ny][nx] = visited[y][x] + 1;
+			q.push({ ny, nx });
+			mx = max(mx, visited[ny][nx]);
+		}
+	}
+}
 
 int main() {
 	scanf("%d%d", &n, &m);
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			scanf(" %c", &a[i][j]);
-			if (a[i][j] == 'L') v.push_back({ i,j });
 		}
 	}
-	int count=0;
-	for (pair<int, int> p : v) {
-		count++; 
-		memset(visited, 0, sizeof(visited));
-		visited[p.first][p.second] = 1;
-		queue<pair<int, int>> q;
-		q.push({ p.first, p.second });
-		while (q.size()) {
-			tie(y, x) = q.front(), q.pop();
-			for (int i = 0; i < 4; i++) {
-				int ny = dy[i] + y;
-				int nx = dx[i] + x;
-				if (ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx] || a[ny][nx] == 'W') continue;
-				visited[ny][nx] = visited[y][x] + 1;
-				q.push({ ny, nx });
-				mx = max(mx, visited[ny][nx]);
-;			}
-		}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) if (a[i][j] == 'L') bfs(i, j);
 	}
 	printf("%d", mx - 1);
 }
