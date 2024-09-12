b5e1f3ae7cff5e29b3f9b90f0b1369fd25641bb5
Haecms <137016400+Haecms@users.noreply.github.com>
2024-09-12 22:36:37 +0900

    [Gold IV] Title: 인구 이동, Time: 72 ms, Memory: 2052 KB -BaekjoonHub

---
+++ 백준/Gold/16234. 인구 이동/인구 이동.cc
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<memory.h>
+using namespace std;
+int n, l, r, a[51][51], visited[51][51], area, area_cnt, area_sum, divide_person[2501];
+int dy[4] = { -1, 0, 1, 0 };
+int dx[4] = { 0, 1, 0, -1 };
+void bfs(int y, int x, int area) {
+	for (int i = 0; i < 4; i++) {
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+		if (ny < 0 || nx < 0 || ny >= n || nx >= n || visited[ny][nx]) continue;
+		if (abs(a[y][x] - a[ny][nx]) < l || abs(a[y][x] - a[ny][nx]) > r) continue;
+		visited[ny][nx] = area;
+		area_cnt++;
+		area_sum += a[ny][nx];
+		bfs(ny, nx, area);
+	}
+}
+int main() {
+	cin >> n >> l >> r;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> a[i][j];
+		}
+	}
+	int cnt = 0;
+	while (true) {
+		area = 1;
+		memset(divide_person, 0, sizeof(divide_person));
+		memset(visited, 0, sizeof(visited));
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (!visited[i][j]) {
+					area_cnt = 1;
+					area_sum = a[i][j];
+					visited[i][j] = area;
+					bfs(i, j, area);
+					divide_person[area] = area_sum / area_cnt;
+					area++;
+				}
+			}
+		}
+		for (int j = 0; j < n; j++) {
+			for (int k = 0; k < n; k++) {
+				a[j][k] = divide_person[visited[j][k]];
+			}
+		}
+		if (area - 1 == n * n) break;
+		cnt++;
+	}
+	cout << cnt;
+}
