289f9af83d50a0257b5b699097a27f9290cb83dc
Haecms <137016400+Haecms@users.noreply.github.com>
2024-09-20 00:33:10 +0900

    [Silver IV] Title: 문자열 접기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/31846. 문자열 접기/문자열 접기.cc
+++ 백준/Silver/31846. 문자열 접기/문자열 접기.cc
@@ -3,14 +3,13 @@ using namespace std;
 int n, q, a, b;
 string s;
 int main() {
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> n >> s >> q;
 	while (q--) {
 		cin >> a >> b;
 		string s_s = "";
-		for (int i = a - 1; i < b; i++) {
+		for (int i = a - 1; i < b; i++) 
 			s_s += string(1, s[i]);
-		}
+		
 		int splitIndex = 1;
 		int realCnt = 0;
 		int splitRange = s_s.size() - 1;
@@ -18,13 +17,12 @@ int main() {
 			int cnt = 0;
 			for (int i = 0; i < splitIndex; i++) {
 				if (splitIndex + i >= s_s.size()) break;
-				int before = s_s[splitIndex - i - 1];
-				int after = s_s[splitIndex + i];
-				if (before == after) cnt++;
+				if (s_s[splitIndex - i - 1] == s_s[splitIndex + i]) cnt++;
 			}
 			realCnt = max(cnt, realCnt);
 			splitIndex++;
 		}
 		cout << realCnt << "\n";
 	}
+	return 0;
 }
