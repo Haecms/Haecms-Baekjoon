e922f7953b60f81e3d9f9b61553b1eff9611ed2c
Haecms <137016400+Haecms@users.noreply.github.com>
2024-09-21 22:55:22 +0900

    [Bronze II] Title: 분산처리, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.cc
+++ 백준/Bronze/1009. 분산처리/분산처리.cc
@@ -1,16 +1,25 @@
 #include<iostream>
+#include<memory.h>
 using namespace std;
-int a, b, c;
+int a, b, c, e[12];
+
 int main() {
 	cin >> a;
-	for (int i = 0; i < a; i++) {
+	while (a--) {
 		cin >> b >> c;
-		int d = 1;
-		for (int j = 0; j < c; j++) {
-			d = (d*b) % 10;
-			if (d == 0) d = 10;
+		int d = b % 10 != 0 ? b % 10 : 10;
+		e[1] = d;
+		int index = 0;
+		for (int j = 2; j <= c + 1; j++) {
+			d = d * b % 10 != 0 ? d * b % 10 : 10;
+			if (e[1] == d) { index = j - 1; break; }
+			else { e[j] = d; }
+			index = j - 1;
 		}
-		cout << d << "\n";
+		if (c % index == 0) cout << e[index] << "\n";
+		else cout << e[c % index] << "\n";
+
+		memset(e, 0, sizeof(e));
 	}
 	return 0;
 }
