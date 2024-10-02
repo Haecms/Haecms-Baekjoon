ff9941b629efff10057ad8872619f138eae45f54
Haecms <137016400+Haecms@users.noreply.github.com>
2024-10-02 22:02:14 +0900

    [Gold V] Title: 내진 설계, Time: 84 ms, Memory: 6948 KB -BaekjoonHub

---
+++ 백준/Gold/31863. 내진 설계/내진 설계.cc
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int n, m, visited[1004][1004], homeCnt = 0, destroiedHomeCnt = 0;
+char a[1004][1004];
+int dy[4] = { 0,-1,1,0 };
+int dx[4] = { 1,0,0,-1 };
+queue<pair<int, int>> destroied;
+
+int main() {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> a[i][j];
+			if (a[i][j] == '#') {
+				homeCnt++;
+				visited[i][j] = 2;
+			}
+			else if (a[i][j] == '*') {
+				homeCnt++;
+				visited[i][j] = 1;
+			}
+			else if (a[i][j] == '@') destroied.push({ i,j });
+		}
+	}
+	while (destroied.size()) {
+		int y = destroied.front().first;
+		int x = destroied.front().second;
+		destroied.pop();
+		for (int i = 0; i < 4; i++) {
+			int ny = dy[i] + y;
+			int nx = dx[i] + x;
+			if (ny < 0 || nx < 0 || ny >= n || nx >= m || a[ny][nx] == '|') continue;
+			if (a[y][x] == '@') {
+				if (ny + dy[i] < 0 || nx + dx[i] < 0 || nx + dx[i] >= m || ny + dy[i] >= n || a[ny + dy[i]][nx + dx[i]] == '|') {
+
+				}
+				else {
+					visited[ny + dy[i]][nx + dx[i]]--;
+					if (!visited[ny + dy[i]][nx + dx[i]]) {
+						homeCnt--;
+						destroiedHomeCnt++;
+						destroied.push({ ny + dy[i],nx + dx[i] });
+					}
+				}
+			}
+			visited[ny][nx]--;
+			if (!visited[ny][nx]) {
+				homeCnt--;
+				destroiedHomeCnt++;
+				destroied.push({ ny,nx });
+			}
+		}
+	}
+	cout << destroiedHomeCnt << " " << homeCnt<< "\n";
+}
