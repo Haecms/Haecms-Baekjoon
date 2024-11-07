387d8bfb1e839cc897475441679ba3669010db34
Haecms <137016400+Haecms@users.noreply.github.com>
2024-11-07 22:00:06 +0900

    [Silver I] Title: 부등호, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.cc
+++ 백준/Silver/2529. 부등호/부등호.cc
@@ -5,7 +5,8 @@
 using namespace std;
 char a[10];
 int k, visited[11];
-vector<string> ret;
+string maxNumber = "000000000", minNumber = "999999999";
+
 bool checkGood(int leftInput, int rightInput, char sign) {
 	if (sign == '<') return leftInput < rightInput;
 	else if (sign == '>') return leftInput > rightInput;
@@ -18,7 +19,8 @@ void dfs(int number, string value, int time) {
 		if (checkGood(number, i, a[time])) {
 			if (time + 1 == k) {
 				visited[number] = 0;
-				ret.push_back(value + to_string(i));
+				maxNumber = max(maxNumber, value + to_string(i));
+				minNumber = min(minNumber, value + to_string(i));
 			}
 			else {
 				dfs(i, value + to_string(i), time + 1);
@@ -31,7 +33,5 @@ int main() {
 	cin >> k;
 	for (int i = 0; i < k; i++) { cin >> a[i]; }
 	for (int i = 0; i < 10; i++) { dfs(i, to_string(i), 0); }	
-	sort(ret.begin(), ret.end());
-	cout << ret[ret.size()-1] << "\n";
-	cout << ret[0] << "\n";
+	cout << maxNumber << "\n" << minNumber << "\n";
 }
