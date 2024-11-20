7f19882b4778786930dd1c341e72dbec0992dc9e
Haecms <137016400+Haecms@users.noreply.github.com>
2024-11-20 23:45:41 +0900

    [Gold IV] Title: 오큰수, Time: 184 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -1,17 +1,21 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<stack>
 using namespace std;
-int n, a[1000004], ret[1000004];
-stack<int> s; //인덱스용
+int n, a, arr[1000004];
+stack<pair<int,int>> s;
 int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> n;
-	memset(ret, -1, sizeof(ret));
-	for (int i = 0; i < n; i++) {
-		cin >> a[i];
-		while (s.size() && a[i] > a[s.top()]) {
-			ret[s.top()] = a[i]; s.pop();
+	for(int i=0; i<n; i++) {
+		cin >> a;
+		while (s.size() && a > s.top().first) {
+			arr[s.top().second] = a;
+			s.pop();
 		}
-		s.push(i);
+		s.push({ a,i });
 	}
-	
-	for (int i = 0; i < n; i++) cout << ret[i] << " ";
+	for (int i = 0; i < n; i++) {
+		if (arr[i] == 0) cout << "-1 ";
+		else cout << arr[i] << " ";
+	}
 }
