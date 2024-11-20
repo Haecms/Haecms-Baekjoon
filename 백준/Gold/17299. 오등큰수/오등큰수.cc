d464d30cd8a9b75139e236dc530e64d79942847e
Haecms <137016400+Haecms@users.noreply.github.com>
2024-11-21 00:26:08 +0900

    [Gold III] Title: 오등큰수, Time: 200 ms, Memory: 18020 KB -BaekjoonHub

--- 백준/Gold/17299. 오등큰수/오등큰수.cc
+++ 백준/Gold/17299. 오등큰수/오등큰수.cc
@@ -1,22 +1,21 @@
 #include<iostream>
 #include<stack>
 using namespace std;
-int n, a, cnt[1000004], original[1000004], ret[1000004];
-stack<pair<int,int>> s;
+int n, cnt[1000004], original[1000004], ret[1000004];
+stack<int> s;
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> n;
 	for (int i = 0; i < n; i++) {
-		cin >> a;
-		original[i] = a;
-		cnt[a]++;
+		cin >> original[i];
+		cnt[original[i]]++;
 	}
 	for (int i = 0; i < n; i++) {
-		while (s.size() && cnt[s.top().first] < cnt[original[i]]) {
-			ret[s.top().second] = original[i];
+		while (s.size() && cnt[original[s.top()]] < cnt[original[i]]) {
+			ret[s.top()] = original[i];
 			s.pop();
 		}
-		s.push({ original[i],i });
+		s.push(i);
 	}
 	for (int i = 0; i < n; i++) {
 		if (ret[i]) cout << ret[i] << " ";
