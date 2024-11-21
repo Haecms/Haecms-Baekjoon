0987e3cb1f02d091e9dc8baa8aaf39f4dae3557f
Haecms <137016400+Haecms@users.noreply.github.com>
2024-11-21 22:00:53 +0900

    [Gold V] Title: 노드사이의 거리, Time: 196 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
@@ -1,24 +1,10 @@
 #include<iostream>
 #include<map>
 #include<vector>
+#include<queue>
 using namespace std;
-int n, m, p1, p2, dis, ret, visited[1001];
+int n, m, p1, p2, dis;
 map<int, vector<pair<int,int>>> connect;
-
-void go(int start, int to, int value) {
-	if (start == to) {
-		ret = min(value, ret);
-		return;
-	}
-	for (int i = 0; i < connect[start].size(); i++) {
-		if (visited[connect[start][i].first]) continue;
-		visited[connect[start][i].first] = 1;
-		value += connect[start][i].second;
-		go(connect[start][i].first, to, value);
-		value -= connect[start][i].second;
-		visited[connect[start][i].first] = 0;
-	}
-}
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
@@ -30,8 +16,23 @@ int main() {
 	}
 	while (m--) {
 		cin >> p1 >> p2;
-		ret = 987654321;
-		go(p1, p2, 0);
-		cout << ret << "\n";
+		queue<pair<int,int>> q;
+		int visited[1001] = {};
+		q.push({ p1, 0 });
+		visited[p1] = 1;
+		while (q.size()) {
+			int start = q.front().first;
+			int dis = q.front().second;
+			q.pop();
+			if (start == p2) {
+				cout << dis << "\n";
+				break;
+			}
+			for (int i = 0; i < connect[start].size(); i++) {
+				if (visited[connect[start][i].first]) continue;
+				visited[connect[start][i].first] = 1;
+				q.push({ connect[start][i].first,  dis + connect[start][i].second });
+			}
+		}
 	}
 }
