d186679a929bc1a0c846603ba5c1f6f6d59f3e38
Haecms <137016400+Haecms@users.noreply.github.com>
2024-11-23 00:02:54 +0900

    [Bronze III] Title: 과민성 대장 증후군, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/31831. 과민성 대장 증후군/과민성 대장 증후군.cc
+++ 백준/Bronze/31831. 과민성 대장 증후군/과민성 대장 증후군.cc
@@ -2,6 +2,7 @@
 using namespace std;
 int n, m, a, ret=0, cnt=0;
 int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> n >> m;
 	for (int i = 0; i < n; i++) {
 		cin >> a;
