fc4466f2f092b676bb3e9210eb376fb83839669c
Haecms <137016400+Haecms@users.noreply.github.com>
2024-11-24 00:19:07 +0900

    [Bronze II] Title: 강의실 예약 시스템, Time: 76 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Bronze/30019. 강의실 예약 시스템/강의실 예약 시스템.cc
+++ 백준/Bronze/30019. 강의실 예약 시스템/강의실 예약 시스템.cc
@@ -1,22 +1,21 @@
 #include<iostream>
-#include<vector>
 using namespace std;
-int n, m, k, s, e;
-vector<int> v[200001];
+int n, m, k, s, e, arr[200001];
+
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> n >> m;
 	while (m--) {
 		cin >> k >> s >> e;
-		if (v[k].size()) {
-			if (v[k].back() <= s) {
-				v[k].push_back(e);
+		if (arr[k]) {
+			if (arr[k] <= s) {
+				arr[k] = e;
 				cout << "YES" << "\n";
 			}
 			else cout << "NO" << "\n";
 		}
 		else {
-			v[k].push_back(e);
+			arr[k] = e;
 			cout << "YES" << "\n";
 		}
 	}
