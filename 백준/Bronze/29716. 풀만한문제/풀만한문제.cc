95ce89328e4db04a9743229376f1ee9aaf61c86b
Haecms <137016400+Haecms@users.noreply.github.com>
2024-11-29 23:43:18 +0900

    [Bronze II] Title: 풀만한문제, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/29716. 풀만한문제/풀만한문제.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int n, m, ret=0;
+string s, stringflash;
+int main() {
+	cin >> n >> m;
+	getline(cin, stringflash);
+	while (m--) {
+		getline(cin, s);
+		int point = 0;
+		for (char c : s) {
+			if (c >= 'A' && c <= 'Z') point += 4;
+			else if (c == ' ') point += 1;
+			else point += 2;
+		}
+		if (point <= n) ret++;
+	}
+	cout << ret << "\n";
+	return 0;
+}
