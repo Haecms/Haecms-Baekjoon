7cd0cce4a3b2682bf6bc5485407bbbf1c0fa2d42
Haecms <137016400+Haecms@users.noreply.github.com>
2024-12-01 22:36:49 +0900

    [Silver IV] Title: 문자열 집합, Time: 52 ms, Memory: 9784 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.cc
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.cc
@@ -1,9 +1,9 @@
 #include<iostream>
-#include<map>
+#include<unordered_map>
 using namespace std;
 int n, m, ret = 0;
 string s;
-map<string, int> ma;
+unordered_map<string, int> ma;
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> n >> m;
