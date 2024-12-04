d99a7b0238de7898886f4494ab47614036843402
Haecms <137016400+Haecms@users.noreply.github.com>
2024-12-05 00:36:03 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.cc
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.cc
@@ -1,45 +1,41 @@
 #include<iostream>
 #include<string>
 using namespace std;
+string s;
+void setData(int startIndex, int endIndex) {
+	while (startIndex < endIndex) {
+		char temp = s[endIndex];
+		s[endIndex--] = s[startIndex];
+		s[startIndex++] = temp;
+	}
+}
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	string s;
 	getline(cin, s);
 	int curPos = 0;
-	string cs = "";
 	while (true) {
 		int arrowPos1 = s.find('<', curPos);
 		int arrowPos2 = s.find('>', arrowPos1);
 		int spacePos = s.find(' ', curPos);
 		if (arrowPos1 == spacePos) {
-			for (int i = s.size() - 1; i >= curPos; i--) {
-				cs += string(1, s[i]);
-			}
+			setData(curPos, s.size() -1);
 			break;
 		}
 		arrowPos1 = arrowPos1 == -1 ? 2000000 : arrowPos1;
 		spacePos = spacePos == -1 ? 2000000 : spacePos;
 		if (arrowPos1 > spacePos) {
-			for (int i = spacePos - 1; i >= curPos; i--) {
-				cs += string(1, s[i]);
-			}
-			cs += " ";
+			setData(curPos, spacePos - 1);
 			curPos = spacePos + 1;
 		}
 		else {
 			if (curPos == arrowPos1) {
-				for (int i = arrowPos1; i <= arrowPos2; i++) {
-					cs += string(1, s[i]);
-				}
 				curPos = arrowPos2 + 1;
 			}
 			else {
-				for (int i = arrowPos1 - 1; i >= curPos; i--) {
-					cs += string(1, s[i]);
-				}
+				setData(curPos, arrowPos1 - 1);
 				curPos = arrowPos1;
 			}
 		}
 	}
-	cout << cs;
+	cout << s;
 }
