a9594ff16965a3f4a9c62d05a12d0d85367278f4
Haecms <137016400+Haecms@users.noreply.github.com>
2024-12-06 00:50:57 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 136 ms, Memory: 6648 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+map<int, vector<int>> ma;
+int n, m, visited[1001], u, x, cnt=0;
+void check(int num) {
+	visited[num] = 1;
+	for (int a : ma[num]) {
+		if (visited[a]) continue;
+		check(a);
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> n >> m;
+	while (m--) {
+		cin >> u >> x;
+		ma[u].push_back(x);
+		ma[x].push_back(u);
+	}
+	for (int i = 1; i <= n; i++) {
+		if (!visited[i]) {
+			cnt++;
+			check(i);
+		}
+	}
+	cout << cnt << "\n";
+	return 0;
+}
