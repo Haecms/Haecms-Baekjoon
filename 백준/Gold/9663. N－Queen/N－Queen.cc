1f1311a0b4086a3073db1d50f5c4c12c085289a0
Haecms <137016400+Haecms@users.noreply.github.com>
2024-12-06 20:23:18 +0900

    [Gold IV] Title: N-Queen, Time: 2624 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<math.h>
+using namespace std;
+int n, visitedX[16], cnt = 0, visited[16][16], curMap[16][16];
+int dy[4] = { 1,1,-1,-1 };
+int dx[4] = { -1,1,-1,1 };
+vector<pair<int, int>> v;
+bool isPossible(int y, int x) {
+    bool check = true;
+    for (auto a : v) {
+        int ry = a.first;
+        int rx = a.second;
+        if (!((abs(ry - y)) - (abs(rx - x)))) return false;
+    }
+    return true;
+}
+void check(int y, int count) {
+    if (count == n) {
+        cnt++;
+        return;
+    }
+    for (int i = 0; i < n; i++) {
+        if (visitedX[i]) continue;
+        if (isPossible(y, i)) {
+            visited[y][i] = 1;
+            visitedX[i] = 1;
+            v.push_back({ y,i });
+
+            check(y + 1, count + 1);
+
+            visited[y][i] = 0;
+            visitedX[i] = 0;
+            v.pop_back();
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> n;
+    check(0, 0);
+    cout << cnt << "\n";
+    return 0;
+}
