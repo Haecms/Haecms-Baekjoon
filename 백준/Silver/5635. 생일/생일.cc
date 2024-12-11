70ef8c6838b5fe6d6ab71cb59ffca74e6b1513d1
Haecms <137016400+Haecms@users.noreply.github.com>
2024-12-11 22:03:06 +0900

    [Silver V] Title: 생일, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/5635. 생일/생일.cc
+++ 백준/Silver/5635. 생일/생일.cc
@@ -9,22 +9,21 @@ struct A {
 	int day;
 	int month;
 	int year;
+
+	bool operator<(const A& x) const {
+		if (year != x.year) return year < x.year;
+		if (month != x.month) return month < x.month;
+		return day < x.day;
+	}
 };
 vector<A> v;
-bool cmp(A a, A b) {
-	if (a.year == b.year) {
-		if (a.month == b.month)  return a.day > b.day; 
-		else return a.month > b.month;
-	}
-	else return a.year > b.year;
-}
 int main() {
 	cin >> n;
 	while (n--) {
 		cin >> s >> d >> m >> y;
 		v.push_back({ s, d, m, y });
 	}
-	sort(v.begin(), v.end(), cmp);
-	cout << v[0].name << "\n";
+	sort(v.begin(), v.end());
 	cout << v[v.size()-1].name << "\n";
+	cout << v[0].name << "\n";
 }
