112936ae4ca54815f49c2d3c20c3a5f0eb6f66d5
Haecms <137016400+Haecms@users.noreply.github.com>
2024-12-16 22:25:27 +0900

    [Silver IV] Title: 30, Time: 4 ms, Memory: 2300 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.cc
+++ 백준/Silver/10610. 30/30.cc
@@ -3,6 +3,7 @@
 using namespace std;
 string s;
 int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> s;
 	sort(s.begin(), s.end(), greater<char>());
 	if (s[s.size() - 1] != '0') cout << -1 << "\n";
