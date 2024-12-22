0a435e945b279a87e9dcc92f3180e82ccb3956db
Haecms <137016400+Haecms@users.noreply.github.com>
2024-12-22 21:12:50 +0900

    [Silver III] Title: 칸토어 집합, Time: 24 ms, Memory: 3184 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.cc
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.cc
@@ -1,15 +1,19 @@
 #include<iostream>
 #include<math.h>
+#include<memory.h>
 using namespace std;
 int n;
 char c[1000004];
 void go(int cnt, int startPoint, int endPoint, int difference) {
-	if (cnt == n) return;
-	for (int i = startPoint; i < startPoint + difference; i++) c[i] = '-';
-	for (int i = startPoint + difference; i < endPoint - difference; i++) c[i] = ' ';
-	for (int i = endPoint - difference; i < endPoint; i++) c[i] = '-';
-	go(cnt + 1, startPoint, startPoint + difference, difference / 3);
-	go(cnt + 1, endPoint - difference, endPoint, difference / 3);
+	if (difference == 1) {
+		c[startPoint] = '-';
+		c[startPoint+1] = ' ';
+		c[startPoint+2] = '-';
+	}
+	else {
+		go(cnt + 1, startPoint, startPoint + difference, difference / 3);
+		go(cnt + 1, endPoint - difference, endPoint, difference / 3);
+	}
 }
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
@@ -21,6 +25,7 @@ int main() {
 			continue;
 		}
 		int r = pow(3, n);
+		memset(c, ' ', c[0] + r);
 		go(0, 0, r, r / 3);
 		for (int i = 0; i < r; i++) cout << c[i];
 		cout << '\n';
