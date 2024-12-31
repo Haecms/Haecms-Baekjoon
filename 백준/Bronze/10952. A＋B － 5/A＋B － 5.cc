43fb645e167ac8b673af14298146f7e885c34ac4
Haecms <137016400+Haecms@users.noreply.github.com>
2025-01-01 03:17:07 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -1,11 +1,9 @@
-#include<iostream>
-using namespace std;
+#include<stdio.h>
 int main() {
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	int a,b;
 	while (true) {
-		cin >> a >> b;
-		if (a == cin.eof() || b == cin.eof()) break;
-		cout << a + b << "\n";
+		scanf("%d %d", &a, &b);
+		if (!a && a == b) break;
+		printf("%d\n", a + b);
 	}
 }
