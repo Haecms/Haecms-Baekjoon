b334f093dde3ab0f10b74ef573a6638a44a8cc22
Haecms <137016400+Haecms@users.noreply.github.com>
2025-01-30 18:48:42 +0900

    [Silver V] Title: 나이순 정렬, Time: 28 ms, Memory: 6900 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -1,23 +1,19 @@
 #include<iostream>
-#include<unordered_map>
 #include<vector>
-#include<algorithm>
 using namespace std;
 int main() {
 	ios_base::sync_with_stdio(0);  cin.tie(0); cout.tie(0);
 	int n, a;
 	string s;
-	unordered_map<int, vector<string>> ma;
 	cin >> n;
+	vector<string> v[201];
 	while (n--) {
 		cin >> a >> s;
-		ma[a].push_back(s);
+		v[a].push_back(s);
 	}
-	vector<pair<int, vector<string>>> v(ma.begin(), ma.end());
-	sort(v.begin(), v.end());
-	for (auto v1 : v) {
-		for (string s : v1.second) {
-			cout << v1.first << " " << s << "\n";
+	for (int i = 1; i < 201; i++) {
+		for (string s : v[i]) {
+			cout << i << " " << s << "\n";
 		}
 	}
 	return 0;
