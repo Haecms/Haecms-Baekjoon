f8266b855a425fd1a4dafbed06b30ee7b8d1bc61
Haecms <137016400+Haecms@users.noreply.github.com>
2025-02-04 21:02:34 +0900

    [Silver I] Title: 그림, Time: 20 ms, Memory: 11664 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -17,6 +17,7 @@ void go(int y, int x) {
 }
 
 int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> n >> m;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) cin >> a[i][j];
