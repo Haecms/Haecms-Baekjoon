ec3a74fcf204c8b79401880deb8721b21772a02c
Haecms <137016400+Haecms@users.noreply.github.com>
2025-02-27 20:45:25 +0900

    [Bronze III] Title: 대지, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.cc
+++ 백준/Bronze/9063. 대지/대지.cc
@@ -3,6 +3,7 @@
 using namespace std;
 int n,y, x, sy=100000000, sx= 100000000, by= -100000000, bx=-100000000;
 int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> n;
 	while (n--) {
 		cin >> y >> x;
