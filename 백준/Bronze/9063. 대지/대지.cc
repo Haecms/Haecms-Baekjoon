757faea30e7ff1edfeec21b3945cc89168897ac8
Haecms <137016400+Haecms@users.noreply.github.com>
2025-02-27 20:44:32 +0900

    [Bronze III] Title: 대지, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int n,y, x, sy=100000000, sx= 100000000, by= -100000000, bx=-100000000;
+int main() {
+	cin >> n;
+	while (n--) {
+		cin >> y >> x;
+		sy = min(y, sy);
+		sx = min(x, sx);
+		by = max(y, by);
+		bx = max(x, bx);
+	}
+	if (sy == by || sx == bx) cout << 0 << "\n";
+	else cout << (by - sy) * (bx - sx) << "\n";
+	return 0;
+}
