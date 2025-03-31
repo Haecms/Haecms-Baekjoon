e22e753df7c587fbc23792a6b4d8402c0054a4c3
Haecms <137016400+Haecms@users.noreply.github.com>
2025-03-31 09:41:18 +0900

    [Gold V] Title: 알약, Time: 68 ms, Memory: 4068 KB -BaekjoonHub

---
+++ 백준/Gold/4811. 알약/알약.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<memory.h>
+using namespace std;
+typedef long long ll;
+ll n, m, dp[64][64][64];
+
+ll go(int idx, int W, int H) {
+	if (idx == m * 2) return 1;
+	ll& ret = dp[idx][W][H];
+	if (ret) return ret;
+	if (W > 0) ret += go(idx + 1, W - 1, H + 1);
+	if(H > 0) ret += go(idx + 1, W, H - 1);
+	return ret;
+}
+
+int main() {
+	while (true) {
+		cin >> m;
+		if (!m) break;
+		memset(dp, 0, sizeof(dp));
+		cout << go(1, m-1, 1) << "\n";
+	}
+	return 0;
+}
