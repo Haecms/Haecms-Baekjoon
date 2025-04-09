092a3f11fef2771d28adf5a168c43663b2789645
Haecms <137016400+Haecms@users.noreply.github.com>
2025-04-09 14:20:21 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -1,13 +1,8 @@
 #include<stdio.h>
 int main() {
-	int n, m, a[10004], v;
-	scanf("%d %d", &n, &m);
-	for (int i=0; i<n*m; i++) a[i] = 0;
-	for (int i=0; i<n*m*2; i++) {
-		scanf("%d", &v);
-		a[i % (n*m)] += v;
-	}
-	for (int i=0; i<n*m; i++) {
-		printf("%d ", a[i]);
-	}
+	int n,m,a[10004],v,i;
+	scanf("%d %d",&n,&m);
+	for (i=0; i<n*m; i++) a[i]=0;
+	for (i=0; i<n*m*2; i++) {scanf("%d",&v); a[i%(n*m)]+=v;}
+	for (i=0; i<n*m; i++) printf("%d ",a[i]);
 }
