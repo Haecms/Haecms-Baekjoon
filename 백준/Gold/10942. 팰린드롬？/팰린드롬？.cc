5da1c00d00cb4660e6be1e4d1b9f3ef626abfecd
Haecms <137016400+Haecms@users.noreply.github.com>
2025-04-10 17:21:27 +0900

    [Gold IV] Title: 팰린드롬?, Time: 224 ms, Memory: 17716 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -1,26 +1,24 @@
 #include<iostream>
-#include<memory.h>
 using namespace std;
 int n, m, a[2004],f,t, dp[2004][2004];
-
-int go(int y, int x) {
-	if (y > x) return 1;
-	int& ret = dp[y][x];
-	if (ret != -1) return ret;
-	ret = 0;
-	if (a[y] == a[x]) ret = go(y + 1, x - 1);
-	return ret;
-}
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> n;
 	for (int i = 1; i <= n; i++) cin >> a[i];
 	cin >> m;
-	memset(dp, -1, sizeof(dp));
+	for (int i = 1; i <= n; i++) dp[i][i] = 1;
+	for (int i = 1; i <= n; i++) {
+		if (a[i] == a[i+1]) dp[i][i + 1] = 1;
+	}
+	for (int i = 2; i <= n; i++) {
+		for (int j = 1; j <= n - i; j++) {
+			if (a[j] == a[i + j] && dp[j + 1][j + i - 1]) dp[j][j + i] = 1;
+		}
+	}
 	while (m--) {
 		cin >> f >> t;
-		cout << go(f,t) << "\n";
+		cout << dp[f][t] << "\n";
 	}
 	return 0;
 }
