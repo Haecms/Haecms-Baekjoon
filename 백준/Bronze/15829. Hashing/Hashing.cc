5aac339e4a8916aaef8431ba4528accd759c3cb3
Haecms <137016400+Haecms@users.noreply.github.com>
2025-04-14 08:33:20 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int n;
+long long ret;
+string s;
+int main() {
+	cin >> n >> s;
+	for (int i = 0; i < n; i++) {
+		long long temp = 1;
+		for (int j = 0; j < i; j++) {
+			temp = temp * 31 % 1234567891;
+		}
+		ret += (s[i] - 96) * temp ;
+	}
+	cout << ret << "\n";
+	return 0;
+}
