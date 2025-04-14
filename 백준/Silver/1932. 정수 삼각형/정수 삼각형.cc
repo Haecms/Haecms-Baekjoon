a0ff32002b8925735011cab79cd030eb19769af2
Haecms <137016400+Haecms@users.noreply.github.com>
2025-04-14 13:50:58 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 3012 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,27 +1,20 @@
 #include<iostream>
-#include<memory.h>
 #include<algorithm>
 using namespace std;
-int n, z, ret, dp[504][504], a[504][504];
-
-int go(int y, int x) {
-	if (y == n-1) return a[y][x];
-	int& ret = dp[y][x];
-	if (ret != -1) return ret;
-	ret = max(ret, go(y + 1, x) + a[y][x]);
-	ret = max(ret, go(y + 1, x+1) + a[y][x]);
-	return ret;
-}
-
+int n, a[504][504];
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> n;
-	fill(&dp[0][0], &dp[0][0] + 504 * 504, -1);
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j <= i; j++) {
 			cin >> a[i][j];
 		}
 	}
-	cout << go(0, 0) << "\n";
+	for (int i = n; i >= 0; i--) {
+		for (int j = 0; j < i; j++) {
+			a[i - 1][j] += max(a[i][j], a[i][j + 1]);
+		}
+	}
+	cout << a[0][0] << "\n";
 	return 0;
 }
