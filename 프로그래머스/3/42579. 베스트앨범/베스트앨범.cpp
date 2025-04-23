6ab69024b824c5826761317f71944e33a6679905
Haecms <137016400+Haecms@users.noreply.github.com>
2025-04-23 13:44:08 +0900

    [level 3] Title: 베스트앨범, Time: 0.04 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
@@ -2,11 +2,8 @@
 #include <vector>
 #include <map>
 #include <algorithm>
-
 using namespace std;
 
-map<string, int> ma;
-map<string, vector<pair<int,int>>> ma2;
 bool cmp1(pair<int, int> a, pair<int, int> b) {
 	return a.second > b.second;
 }
@@ -16,6 +13,9 @@ bool cmp2(pair<string, int> a, pair<string, int> b) {
 
 vector<int> solution(vector<string> genres, vector<int> plays) {
     vector<int> answer;
+    map<string, int> ma;
+    map<string, vector<pair<int,int>>> ma2;
+    
     for (int i = 0; i < genres.size(); i++) {
 		ma[genres[i]] += plays[i];
 		ma2[genres[i]].push_back({ i, plays[i] });
@@ -24,6 +24,7 @@ vector<int> solution(vector<string> genres, vector<int> plays) {
         
 		if (ma2[genres[i]].size() > 2) ma2[genres[i]].pop_back();
 	}
+    
 	vector<pair<string, int>> v_genres(ma.begin(), ma.end());
 	sort(v_genres.begin(), v_genres.end(), cmp2);
     
@@ -32,5 +33,6 @@ vector<int> solution(vector<string> genres, vector<int> plays) {
 			answer.push_back(ma2[p.first][i].first);
 		}
 	}
+    
     return answer;
 }
