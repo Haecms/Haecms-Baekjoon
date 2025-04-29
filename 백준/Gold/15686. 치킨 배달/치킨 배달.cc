724d601fb3de742c942c8ea708e94b08de07ab78
Haecms <137016400+Haecms@users.noreply.github.com>
2025-04-29 10:27:41 +0900

    [Gold V] Title: 치킨 배달, Time: 0 ms, Memory: 2312 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,46 +1,47 @@
 #include<iostream>
 #include<vector>
+#include<math.h>
 using namespace std;
-int n, m, a[51][51], visited[51][51], result = 987654321;
-vector<vector<int>> chickenList;
-vector<pair<int, int>> chicken, _home;
+int n, m, a[51][51];
+vector<pair<int, int>> home;
+vector<pair<int,int>> v1;
+vector<vector<pair<int,int>>> v_chicken;
 
-void combi(int start, vector<int> v) {
-	if (v.size() == m) {
-		chickenList.push_back(v);
-		return;
+void go(int idx, vector<pair<int,int>>& v2) {
+	if (v2.size() == m) v_chicken.push_back(v2);
+	else {
+		for (int i = idx; i < v1.size(); i++) {
+			v2.push_back(v1[i]);
+			go(i + 1, v2);
+			v2.pop_back();
+		}
 	}
-	for (int i = start; i < chicken.size(); i++) {
-		v.push_back(i);
-		combi(i+1, v);
-		v.pop_back();
-	}
-	return;
 }
+
 int main() {
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	ios_base::sync_with_stdio(0); cin.tie(0);
 	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
 			cin >> a[i][j];
-			if (a[i][j] == 1) _home.push_back({ i,j });
-			if (a[i][j] == 2) chicken.push_back({ i,j });
+			if (a[i][j] == 2) v1.push_back({ i,j });
+			else if (a[i][j]) home.push_back({ i,j });
 		}
 	}
-	vector<int> v;
-	combi(0, v);
-	for (vector<int> cList : chickenList) {
-		int ret = 0;
-		for (pair<int, int> home : _home) {
-			int _min = 987654321;
-			for (int ch : cList) {
-				int _dist = abs(home.first - chicken[ch].first) + abs(home.second - chicken[ch].second);
-				_min = min(_min, _dist);
+	vector<pair<int, int>> v2;
+	go(0, v2);
+	int ret = 20000000;
+	for (int i = 0; i < v_chicken.size(); i++) {
+		int cnt = 0;
+		for (pair<int, int> house : home) {
+			int distance = 20000000;
+			for (pair<int, int> chicken : v_chicken[i]) {
+				distance = min(distance, abs(house.first - chicken.first) + abs(house.second - chicken.second));
 			}
-			ret += _min;
+			cnt += distance;
 		}
-		result = min(result, ret);
+		ret = min(cnt, ret);
 	}
-	cout << result << "\n";
+	cout << ret << "\n";
 	return 0;
 }
