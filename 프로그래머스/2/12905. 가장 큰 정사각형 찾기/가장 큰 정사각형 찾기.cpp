11435d3484fd8b11840b4786665939886f3d42e6
Haecms <137016400+Haecms@users.noreply.github.com>
2025-04-30 13:16:50 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 8.12 ms, Memory: 42.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+int a[1002][1002];
+int solution(vector<vector<int>> board)
+{
+    int answer = 1234;
+
+	for (int i = 1; i <= board.size(); i++) {
+		for (int j = 1; j <= board[i-1].size(); j++) {
+			a[i][j] = board[i - 1][j - 1];
+			a[i][j] += a[i - 1][j] + a[i][j - 1] - a[i-1][j-1];
+		}
+	}
+    
+	int size = min(board.size(), board[0].size());
+	while (true) {
+		for (int i = board.size(); i >= size; i--) {
+			for (int j = board[i-1].size(); j >= size; j--) {
+				if (a[i][j] + a[i - size][j - size] - a[i - size][j] - a[i][j - size] == size * size) {
+					answer = size * size;
+					return answer;
+				}
+			}
+		}
+		size--;
+	}
+}
