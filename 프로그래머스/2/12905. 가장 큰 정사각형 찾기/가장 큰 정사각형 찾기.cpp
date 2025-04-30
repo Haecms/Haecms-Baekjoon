6af6d8583e307d148864a8394f8293ed0d8244e5
Haecms <137016400+Haecms@users.noreply.github.com>
2025-04-30 13:41:05 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 7.63 ms, Memory: 42.6 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
@@ -1,22 +1,21 @@
-#include <iostream>
-#include<vector>
+#include <vector>
+#include <algorithm>
 using namespace std;
-int a[1002][1002];
 int solution(vector<vector<int>> board)
 {
-    int answer = 1234;
+	int answer = 1234;
+	int a[1002][1002];
 
 	for (int i = 1; i <= board.size(); i++) {
-		for (int j = 1; j <= board[i-1].size(); j++) {
-			a[i][j] = board[i - 1][j - 1];
-			a[i][j] += a[i - 1][j] + a[i][j - 1] - a[i-1][j-1];
+		for (int j = 1; j <= board[i - 1].size(); j++) {
+			a[i][j] = a[i - 1][j] + a[i][j - 1] - a[i - 1][j - 1] + board[i - 1][j - 1];
 		}
 	}
-    
+
 	int size = min(board.size(), board[0].size());
 	while (true) {
 		for (int i = board.size(); i >= size; i--) {
-			for (int j = board[i-1].size(); j >= size; j--) {
+			for (int j = board[i - 1].size(); j >= size; j--) {
 				if (a[i][j] + a[i - size][j - size] - a[i - size][j] - a[i][j - size] == size * size) {
 					answer = size * size;
 					return answer;
