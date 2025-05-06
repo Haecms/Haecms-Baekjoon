27c75d3dd786625807878cafb6c7f87a78957c09
Haecms <137016400+Haecms@users.noreply.github.com>
2025-05-07 08:42:15 +0900

    [Gold IV] Title: 알파벳, Time: 348 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,28 +1,34 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
-int R, C, visited[30], cnt=0;
-char a[24][24];
-int dy[4] = { 0,-1,0,1 };
-int dx[4] = { -1,0,1,0 };
-void dfs(int y, int x, int count){
+int n, m, a[24][24], visited[100], ret;
+int dy[4] = { 0,1,0,-1 };
+int dx[4] = { 1,0,-1,0 };
+char c;
+
+void dfs(int y, int x, int cnt) {
 	for (int i = 0; i < 4; i++) {
-		int ny = dy[i] + y;
-		int nx = dx[i] + x;
-		if (ny < 0 || nx < 0 || ny >= R || nx >= C || visited[a[ny][nx] - 'A']) continue;
-		visited[a[ny][nx] - 'A'] = 1;
-		dfs(ny, nx, count + 1);
-		visited[a[ny][nx] - 'A'] = 0;
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+		if (ny < 0 || nx < 0 || ny >= n || nx >= m || visited[a[ny][nx]]) continue;
+		visited[a[ny][nx]] = 1;
+		dfs(ny, nx, cnt + 1);
+		visited[a[ny][nx]] = 0;
 	}
-	cnt = max(count, cnt);
+	ret = max(ret, cnt);
 }
+
 int main() {
-	cin >> R >> C;
-	for (int i = 0; i < R; i++){
-		for (int j = 0; j < C; j++) {
-			cin >> a[i][j];
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> c;
+			a[i][j] = c - 'A';
 		}
 	}
-	visited[a[0][0] - 'A'] = 1;
-	dfs(0,0,1);
-	cout << cnt << "\n";
+	visited[a[0][0]] = 1;
+	dfs(0, 0, 1);
+	cout << ret << "\n";
+	return 0;
 }
