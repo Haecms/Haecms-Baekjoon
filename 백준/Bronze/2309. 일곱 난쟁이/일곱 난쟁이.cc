35e19125381a87229bcdf70f6b0a773c05ff8d6d
Haecms <137016400+Haecms@users.noreply.github.com>
2025-05-16 13:10:45 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,40 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void hello(vector<int> a) {
-	for (int i = 0; i <= a.size() - 7; i++) {
-		for (int j = i + 1; j <= a.size() - 6; j++) {
-			for (int k = j + 1; k <= a.size() - 5; k++) {
-				for (int l = k + 1; l <= a.size() - 4; l++) {
-					for (int m = l + 1; m <= a.size() - 3; m++) {
-						for (int n = m + 1; n <= a.size() - 2; n++) {
-							for (int o = n + 1; o <= a.size() - 1; o++) {
-								if (a[i] + a[j] + a[k] + a[l] + a[m] + a[n] + a[o] == 100) {
-									vector<int> answer = { a[i], a[j],a[k],a[l],a[m],a[n],a[o] };
-									sort(answer.begin(), answer.end());
-									cout << answer[0] << endl;
-									cout << answer[1] << endl;
-									cout << answer[2] << endl;
-									cout << answer[3] << endl;
-									cout << answer[4] << endl;
-									cout << answer[5] << endl;
-									cout << answer[6] << endl;
-									return;
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-}
 int main() {
-	vector<int> a;
-	int value;
+	int a[9];
 	for (int i = 0; i < 9; i++) {
-		cin >> value;
-		a.push_back(value);
+		cin >> a[i];
 	}
-	hello(a);
+	sort(a, a + 9);
+	do {
+		int sum = 0;
+		for (int i = 0; i < 7; i++) sum += a[i];
+		if (sum == 100) break;
+	} while (next_permutation(a, a + 9));
+	for (int i = 0; i < 7; i++) cout << a[i] << endl;
 }
