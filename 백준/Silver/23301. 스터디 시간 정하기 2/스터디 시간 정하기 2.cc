d2e579a28c157ebdfb274aed7badea856306413a
Haecms <137016400+Haecms@users.noreply.github.com>
2025-05-18 00:39:33 +0900

    [Silver IV] Title: 스터디 시간 정하기 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/23301. 스터디 시간 정하기 2/스터디 시간 정하기 2.cc
+++ 백준/Silver/23301. 스터디 시간 정하기 2/스터디 시간 정하기 2.cc
@@ -1,37 +1,22 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-int n, t, k, s, e, max_satisfy_score = 0, max_time=0;
-vector<pair<int, int>> v;
-vector<pair<int, int>> good_time;
+#include<stdio.h>
+int n, m, k, f, t, a[1004], maxSum, ret;
 int main() {
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	cin >> n >> t;
-	for (int i = 0; i < n; i++) {
-		cin >> k;
+	scanf("%d %d", &n, &m);
+	while (n--) {
+		scanf("%d", &k);
 		while (k--) {
-			cin >> s >> e;
-			v.push_back({ s, e });
-			if (e > max_time) max_time = e;
+			scanf("%d %d", &f, &t);
+			for (int i = f; i < t; i++) a[i]++;
 		}
 	}
-	int start_time = 0;
-	while (start_time + t <= max_time) {
-		int satisfy_score = 0;
-		int end_time = start_time + t;
-		for (auto time : v) {
-			int study_start_time = max(start_time, time.first);
-			int study_end_time = min(end_time, time.second);
-			if (study_start_time < study_end_time && study_start_time < end_time) {
-				satisfy_score += study_end_time - study_start_time;
-			}
+	for (int i = 0; i+m < 1001; i++) {
+		int sum = 0;
+		for (int j = i; j < i + m; j++) sum += a[j];
+		if (sum > maxSum) {
+			ret = i;
+			maxSum = sum;
 		}
-		if (max_satisfy_score < satisfy_score) {
-			max_satisfy_score = satisfy_score;
-			if (good_time.size() > 0) good_time.pop_back();
-			good_time.push_back({ start_time, end_time });
-		}
-		start_time++;
 	}
-	cout << good_time[0].first << " " << good_time[0].second << "\n";
+	printf("%d %d", ret, ret + m);
+	return 0;
 }
