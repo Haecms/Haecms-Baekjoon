2b69fa80b0fd45f9581009d69aa58203105f6438
Haecms <137016400+Haecms@users.noreply.github.com>
2025-05-19 21:33:35 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -2,30 +2,22 @@
 #include<queue>
 using namespace std;
 int n, m, visited[100004];
-queue<pair<int, int>> q;
+queue<int> q;
 int main() {
 	cin >> n >> m;
-	q.push({ n,1 });
+	q.push(n);
+	visited[n] = 1;
 	while (q.size()) {
-		int cur = q.front().first;
-		int cnt = q.front().second;
+		int cur = q.front();
 		q.pop();
 		if (cur == m) {
-			cout << cnt-1 << "\n";
+			cout << visited[cur]-1 << "\n";
 			break;
 		}
-		visited[cur] = 1;
-		if (cur + 1 <= 100001 && !visited[cur + 1]) {
-			q.push({ cur + 1, cnt + 1 });
-			visited[cur + 1] = 1;
-		}
-		if (cur - 1 >= 0 && !visited[cur - 1]) {
-			q.push({ cur - 1, cnt + 1 });
-			visited[cur - 1] = 1;
-		}
-		if (cur * 2 <= 100001 && !visited[cur * 2]) {
-			q.push({ cur * 2, cnt + 1 });
-			visited[cur * 2] = 1;
+		for (int a : {cur + 1, cur - 1, cur * 2}) {
+			if (a < 0 || a > 100000 || visited[a]) continue;
+			visited[a] = visited[cur]+1;
+			q.push(a);
 		}
 	}
 	return 0;
