09f1506d3e966b8e12ed7c0be7f95849f961b86e
Haecms <137016400+Haecms@users.noreply.github.com>
2025-05-21 21:07:07 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 4 ms, Memory: 3732 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -1,37 +1,36 @@
 #include<iostream>
 #include<queue>
 using namespace std;
-int subin, brother, visited[400004], cnt[400004], minTime=2000000;
-queue<int> q;
+int n, m, maxCnt = 1e9, ret, visited[200004];
+queue<pair<int, int>> q;
 int main() {
-	cin >> subin >> brother;
-	q.push(subin);
-	visited[subin] = 1;
+	for (int i = 0; i < 200004; i++) visited[i] = 1e9;
+	cin >> n >> m;
+	q.push({ n,1 });
+	visited[n] = 1;
 	while (q.size()) {
-		int subinPlace = q.front();
-		if (subinPlace == brother) {
-			minTime = min(minTime, visited[subinPlace] - 1);
-			cnt[visited[subinPlace] - 1]++;
-			q.pop();
+		int cur = q.front().first;
+		int cnt = q.front().second;
+		q.pop();
+		if (maxCnt < cnt) break;
+		if (cur == m) {
+			maxCnt = cnt;
+			ret++;
 			continue;
 		}
-		q.pop();
-		int go_up = subinPlace + 1;
-		int go_down = subinPlace - 1;
-		int go_telepote = subinPlace * 2;
-		if (go_up >= 0 && go_up <= 200001 && (!visited[go_up] || visited[go_up] >= visited[subinPlace] + 1)) {
-			visited[go_up] = visited[subinPlace] + 1;
-			q.push(go_up);
+		if (cur - 1 >= 0 && visited[cur - 1] >= cnt + 1) {
+			visited[cur - 1] = cnt + 1;
+			q.push({ cur - 1, cnt + 1 });
 		}
-		if (go_down >= 0 && go_down <= 200001 && (!visited[go_down] || visited[go_down] >= visited[subinPlace] + 1)) {
-			visited[go_down] = visited[subinPlace] + 1;
-			q.push(go_down);
+		if (cur + 1 <= 100000 && visited[cur + 1] >= cnt + 1) {
+			visited[cur + 1] = cnt + 1;
+			q.push({ cur + 1, cnt + 1 });
 		}
-		if (go_telepote >= 0 && go_telepote <= 200001 && (!visited[go_telepote] || visited[go_telepote] >= visited[subinPlace] + 1)) {
-			visited[go_telepote] = visited[subinPlace] + 1;
-			q.push(go_telepote);
+		if (cur * 2 <= 200004 && visited[cur * 2] >= cnt * 1) {
+			visited[cur * 2] = cnt + 1;
+			q.push({ cur * 2, cnt + 1 });
 		}
 	}
-	cout << minTime << "\n" << cnt[minTime] << "\n";
+	cout << maxCnt-1 << "\n" << ret << "\n";
 	return 0;
 }
