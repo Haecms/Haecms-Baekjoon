5d77cdbc6292f7a2de71ff7091a227753b718535
Haecms <137016400+Haecms@users.noreply.github.com>
2025-07-12 21:57:46 +0900

    [Silver IV] Title: 대칭 차집합, Time: 160 ms, Memory: 21428 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -1,17 +1,13 @@
 #include<iostream>
 #include<unordered_map>
 using namespace std;
+int n, m, a, ret, b;
+unordered_map<int, int> um;
 int main() {
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int n, m, a, ret = 0;
-	unordered_map<int, int> ma;
-	cin >> n >> m;
-	for (int i = 0; i < n + m; i++) {
-		cin >> a;
-		ma[a]++;
-	}
-	for (auto m : ma) {
-		if (m.second == 1) ret++;
-	}
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	cin >> n >> m; b = n + m;
+	while (b--) cin >> a, um[a]++;
+	for (auto i : um) if (i.second & 1) ret++;
 	cout << ret << "\n";
+	return 0;
 }
