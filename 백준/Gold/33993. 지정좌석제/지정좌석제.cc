b2916fe3a6f7c601a456d4f25fcc8d3be7c8254d
Haecms <137016400+Haecms@users.noreply.github.com>
2025-07-12 21:43:33 +0900

    [Gold V] Title: 지정좌석제, Time: 184 ms, Memory: 99832 KB -BaekjoonHub

--- 백준/Gold/33993. 지정좌석제/지정좌석제.cc
+++ 백준/Gold/33993. 지정좌석제/지정좌석제.cc
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
-int n, r, c, w, a[5004][5004],o,p, aa[5004][5004], ret;
+int n, r, c, w, o, p, a[5004][5004], ret, retX, retY;
 pair<int, int> retp;
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0);
@@ -9,27 +9,26 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		cin >> o >> p;
 		a[o][p] = 1;
-		aa[o][p] = 1;
 	}
 	for (int i = 1; i <= r; i++) {
 		for (int j = 1; j <= c; j++) {
-			aa[i][j] += aa[i - 1][j] + aa[i][j - 1] - aa[i-1][j-1];
+			a[i][j] += a[i - 1][j] + a[i][j - 1] - a[i-1][j-1];
 		}
 	}
 	for (int x = 1; x <= r; x++) {
 		for (int y = 1; y <= c; y++) {
-			if (a[x][y]) continue;
+			if (a[x][y] + a[x - 1][y - 1] - a[x][y - 1] - a[x - 1][y]) continue;
 			int upY = min(y + w / 2, c);
 			int upX = min(x + w / 2, r);
 			int downY = max(y - w / 2 - 1, 0);
 			int downX = max(x - w / 2 - 1, 0);
-			if (ret < aa[upX][upY] + aa[downX][downY] - aa[upX][downY] - aa[downX][upY]) {
-				ret = aa[upX][upY] + aa[downX][downY] - aa[upX][downY] - aa[downX][upY];
-				retp.first = x;
-				retp.second = y;
+			if (ret < a[upX][upY] + a[downX][downY] - a[upX][downY] - a[downX][upY]) {
+				ret = a[upX][upY] + a[downX][downY] - a[upX][downY] - a[downX][upY];
+				retX = x;
+				retY = y;
 			}
 		}
 	}
-	cout << ret << "\n" << retp.first << " " << retp.second << "\n";
+	cout << ret << "\n" << retX << " " << retY << "\n";
 	return 0;
 }
