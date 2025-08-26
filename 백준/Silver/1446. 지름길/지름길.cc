d8ec4b749c9b229ff4afcf11e56198c6309aab71
Haecms <137016400+Haecms@users.noreply.github.com>
2025-08-26 20:47:43 +0900

    [Silver I] Title: 지름길, Time: 0 ms, Memory: 2296 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.cc
+++ 백준/Silver/1446. 지름길/지름길.cc
@@ -1,48 +1,25 @@
 #include<iostream>
 #include<vector>
-#include<queue>
-#include<algorithm>
+#include<climits>
 using namespace std;
-int n, m, visited[10004], a,b,c;
+int n, m, a,b,c, visited[10004];
 vector<pair<int, int>> v[10004];
-struct A {
-	int cur, sum;
-	bool operator<(const A& a) const {
-		return sum > a.sum;
-	}
-};
-priority_queue<A> pq;
-bool cmp(pair<int,int> a, pair<int,int> b) {
-	return a.second < b.second;
-}
 int main() {
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	ios_base::sync_with_stdio(0); cin.tie(0);
 	cin >> n >> m;
+	for (int i = 1; i <= m; i++) visited[i] = INT_MAX;
 	for (int i = 0; i < n; i++) {
 		cin >> a >> b >> c;
+		if (b > m) continue;
 		v[a].push_back({ b,c });
 	}
-	for (int i = 0; i <= n; i++) {
-		sort(v[i].begin(), v[i].end(), cmp);
-	}
-	pq.push({ 0,0 });
-	while (pq.size()) {
-		int cur = pq.top().cur;
-		int sum = pq.top().sum;
-		pq.pop();
-		if (cur > m) continue;
-		if (cur == m) break;
-		if (v[cur].size()) {
-			for (int i = 0; i < v[cur].size(); i++) {
-				if (!visited[v[cur][i].first] || visited[v[cur][i].first] > sum + v[cur][i].second) {
-					pq.push({ v[cur][i].first, sum + v[cur][i].second });
-					visited[v[cur][i].first] = sum + v[cur][i].second;
-				}
+	for (int i = 0; i <= m; i++) {
+		if (i) visited[i] = min(visited[i], visited[i - 1] + 1);
+		if (v[i].size()) {
+			for (auto d : v[i]) {
+				visited[d.first] = min(visited[d.first], visited[i] + d.second);
 			}
 		}
-		if (visited[cur + 1] && visited[cur + 1] <= sum + 1) continue;
-		pq.push({ cur + 1, sum + 1 });
-		visited[cur + 1] = sum + 1;
 	}
 	cout << visited[m] << "\n";
 	return 0;
