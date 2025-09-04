cb13a821f9da7bbf2340c4670830a86b0ace892f
Haecms <137016400+Haecms@users.noreply.github.com>
2025-09-05 08:50:05 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -1,21 +1,20 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
-string s, ori_s, pre, suf;
 int n;
-
-int main()
-{
-	cin >> n;
-	cin >> ori_s;
-	int pos = ori_s.find('*');
-	pre = ori_s.substr(0, pos);
-	suf = ori_s.substr(pos + 1);
-	for (int i = 0; i < n; i++) {
+string s;
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> n >> s;
+	int starIdx = s.find('*');
+	string before = s.substr(0, starIdx);
+	string after = s.substr(starIdx+1);
+	while (n--) {
 		cin >> s;
-		if (pre.size() + suf.size() > s.size()) cout << "NE\n";
-		else {
-			if (pre == s.substr(0, pre.size()) && suf == s.substr(s.size() - suf.size())) cout << "DA\n";
-			else cout << "NE\n";
-		}
+		if (s.size() < before.size() + after.size()) { cout << "NE\n"; continue; }
+		string sBefore = s.substr(0, before.size());
+		string sAfter = s.substr(s.size() - after.size());
+		if (sBefore == before && sAfter == after) cout << "DA\n";
+		else cout << "NE\n";
 	}
+	return 0;
 }
