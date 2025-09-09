7b34b89c280ee2f723257006708f2cdc71a5e09a
Haecms <137016400+Haecms@users.noreply.github.com>
2025-09-09 13:07:47 +0900

    [Silver I] Title: 영역 구하기, Time: 0 ms, Memory: 2252 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.cc
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.cc
@@ -1,47 +1,44 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
-const int max_n = 101;
-int a[max_n][max_n], visited[max_n][max_n];
-int N, M, K, l_x, l_y, r_x, r_y, ny, nx;
-int dy[4] = { -1, 0, 1, 0 };
-int dx[4] = { 0, 1, 0, -1 };
+int n, m, k, a,b,c,d, e[104][104], cnt;
+int dy[4] = { 0,1,0,-1 };
+int dx[4] = { 1,0,-1,0 };
 vector<int> ret;
-
-int dfs(int y, int x) {
-	visited[y][x] = 1;
-	int ret = 1;
+void go(int y, int x) {
+	e[y][x]++;
+	cnt++;
 	for (int i = 0; i < 4; i++) {
-		ny = y + dy[i];
-		nx = x + dx[i];
-		if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
-		if (visited[ny][nx] || a[ny][nx]) continue;
-		ret += dfs(ny, nx);
+		int ny = dy[i] + y;
+		int nx = dx[i] + x;
+		if (ny < 0 || nx < 0 || ny >= n || nx >= m || e[ny][nx])continue;
+		go(ny, nx);
 	}
-	return ret;
 }
- 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-	cin >> N >> M >> K;
-
-	for (int i = 0; i < K; i++) {
-		cin >> l_x >> l_y >> r_x >> r_y;
-		for (int j = l_y; j < r_y; j++) {
-			for (int k = l_x; k < r_x; k++) {
-				a[j][k] = 1;
+	cin >> n >> m >> k;
+	while (k--) {
+		cin >> a >> b >> c >> d;
+		for (int i = b; i < d; i++) {
+			for (int j = a; j < c; j++) {
+				e[i][j]++;
 			}
 		}
 	}
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			if (!visited[i][j] && !a[i][j]) {
-				ret.push_back(dfs(i, j));
-				
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (!e[i][j]) {
+				cnt = 0;
+				go(i, j);
+				ret.push_back(cnt);
 			}
 		}
 	}
 	sort(ret.begin(), ret.end());
 	cout << ret.size() << "\n";
-	for (int a : ret) cout << a << " ";
+	for (int vv : ret) {
+		cout << vv << " ";
+	}
+	return 0;
 }
