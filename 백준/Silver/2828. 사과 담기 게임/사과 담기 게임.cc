86545e328e5773bfad25660520b09213d87ee3aa
Haecms <137016400+Haecms@users.noreply.github.com>
2025-09-09 14:43:39 +0900

    [Silver V] Title: 사과 담기 게임, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.cc
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.cc
@@ -1,20 +1,12 @@
-#include<bits/stdc++.h>
-using namespace std;
-int n, m, k, cp, fp, ret = 0;
-int main(){
-	cin >> n >> m >> k;
-	cp = 1;
-	for (int i = 0; i < k; i++) {
-		cin >> fp;
-		if (fp >= cp && fp <= cp + m - 1) continue;
-		if (fp < cp) {
-			ret += cp - fp;
-			cp = fp;
-		}
-		else {
-			ret += fp - cp - m + 1;
-			cp = fp - m + 1;
-		}
+#include<stdio.h>
+int n, j, a, li=1, ri, ret;
+int main() {
+	scanf("%d%d%d", &n, &ri, &j);
+	while (j--) {
+		scanf("%d", &a);
+		while (li > a) { ret++; li--; ri--; }
+		while (ri < a) { ret++; li++; ri++; }
 	}
-	cout << ret << "\n";
+	printf("%d", ret);
+	return 0;
 }
