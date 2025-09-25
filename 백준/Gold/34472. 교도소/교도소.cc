11b0eb277c1aee552fecfff9d91a0b5ba3f8fa49
Haecms <137016400+Haecms@users.noreply.github.com>
2025-09-25 09:47:59 +0900

    [Gold IV] Title: 교도소, Time: 28 ms, Memory: 5928 KB, Score: 42 point -BaekjoonHub

--- 백준/Gold/34472. 교도소/교도소.cc
+++ 백준/Gold/34472. 교도소/교도소.cc
@@ -1,47 +1,27 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 typedef long long ll;
-ll n, a, arr[500004], need, sum, ret,rest;
+ll n, a, arr[500004], need, sum, ret, rest;
 int main() {
-    ios_base::sync_with_stdio(0); cin.tie(0);
+	ios_base::sync_with_stdio(0); cin.tie(0);
 	cin >> n;
 	for (int i = 0; i < n; i++) {
 		cin >> arr[i];
 		sum += arr[i];
 	}
 	need = sum / n;
-	ll overSum = 0;
-	ll startIdx = 0;
-	for (int i = n - 1, j=1; i >= 0; i--,j++) {
-		overSum += arr[i];
-		if (overSum > j * need) {
-			startIdx = i;
-			break;
-		}
-	}
-	for (int i = startIdx; i < n; i++) {
-		ret += rest;
-		if (arr[i] > need) {
-			rest += arr[i] - need;
-			arr[i] = need;
-		}
-		else if(arr[i] < need){
-			rest -= (need - arr[i]);
-			arr[i] = need;
-		}
-	}
-	arr[0] += rest;
-	ret += rest;
-	rest = 0;
-	for (int i = 0; i < n; i++) {
-		ret += rest;
-		if (arr[i] > need) {
-			rest += arr[i] - need;
-			arr[i] = need;
-		}
-		else if(arr[i] < need){
-			rest -= (need - arr[i]);
-			arr[i] = need;
+	for (int k = 0; k < 2; k++) {
+		for (int i = 0; i < n; i++) {
+			ret += rest;
+			if (arr[i] > need) {
+				rest += (arr[i] - need);
+				arr[i] = need;
+			}
+			else if (arr[i] < need && rest + arr[i] >= need) {
+				rest -= (need - arr[i]);
+				arr[i] = need;
+			}
 		}
 	}
 	cout << ret << "\n";
