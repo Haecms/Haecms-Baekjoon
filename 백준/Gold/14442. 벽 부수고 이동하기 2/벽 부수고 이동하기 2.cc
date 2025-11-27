3714b3c59a4715a4e273383ffb1ca8b35bb0c757
Haecms <137016400+Haecms@users.noreply.github.com>
2025-11-27 16:03:39 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 272 ms, Memory: 15108 KB -BaekjoonHub

---
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.cc
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int n, m, k, a[1004][1004], visited[1004][1004], macnt[1004][1004];
+int dy[4] = { -1,0,1,0 };
+int dx[4] = { 0,1,0,-1 };
+char c;
+struct A {
+	int y;
+	int x;
+	int cnt;
+};
+queue<A> q;
+
+void go() {
+	while (q.size()) {
+		int y = q.front().y;
+		int x = q.front().x;
+		int cnt = q.front().cnt;
+		q.pop();
+		if (macnt[y][x] < cnt) continue;
+		if (y == n-1 && x == m-1) return;
+		for (int i = 0; i < 4; i++) {
+			int ny = dy[i] + y;
+			int nx = dx[i] + x;
+			if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+			if (cnt < k && a[ny][nx]) {
+				if (!visited[ny][nx] || macnt[ny][nx] > cnt + 1) {
+					visited[ny][nx] = visited[y][x] + 1;
+					macnt[ny][nx] = cnt + 1;
+					q.push({ ny,nx, cnt + 1 });
+				}
+			}
+			if (a[ny][nx]) continue;
+			if (visited[ny][nx] && macnt[ny][nx] <= cnt) continue;
+			visited[ny][nx] = visited[y][x] + 1;;
+			macnt[ny][nx] = cnt;
+			q.push({ ny,nx, cnt });
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> n >> m >> k;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> c;
+			a[i][j] = c - '0';
+		}
+	}
+	visited[0][0] = 1;
+	q.push({ 0,0,0 });
+	go();
+	if (visited[n - 1][m - 1]) cout << visited[n - 1][m - 1];
+	else cout << -1;
+}
