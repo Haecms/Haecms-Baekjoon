17c7a11ca06e3beb61c321ded235c10417142149
Haecms <137016400+Haecms@users.noreply.github.com>
2025-11-28 13:49:54 +0900

    [Silver V] Title: 단어 정렬, Time: 44 ms, Memory: 7316 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -1,23 +1,30 @@
 #include<iostream>
-#include<unordered_set>
+#include<unordered_map>
 #include<vector>
 #include<algorithm>
+
 using namespace std;
+
 int n;
+unordered_map<string, int>um;
 string s;
-unordered_set<string> us;
-bool cmp(string a, string b) {
-	if (a.size() != b.size()) return a.size() < b.size();
-	return a < b;
+vector<string> v;
+
+bool cmp(string s1, string s2) {
+	if (s1.size() != s2.size()) return s1.size() < s2.size();
+	return s1 < s2;
 }
+
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
 	cin >> n;
-	while (n--) {
-		cin >> s;
-		us.insert(s);
-	}
-	vector<string> v(us.begin(), us.end());
+	while (n--) { cin >> s; um[s]++; }
+
+	for (auto ma : um) { v.push_back(ma.first); }
 	sort(v.begin(), v.end(), cmp);
-	for (auto a : v) cout << a << "\n";
+
+	for (string vs : v) cout << vs << "\n";
+
+	return 0;
 }
