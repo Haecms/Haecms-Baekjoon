b3cea573de7671e456121e941874d17e07bf3d93
Haecms <137016400+Haecms@users.noreply.github.com>
2025-12-31 09:08:37 +0900

    [Bronze V] Title: 채플이 너무해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34945. 채플이 너무해/채플이 너무해.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    
+    cin >> n;
+    
+    if(n>=6) cout << "Success!";
+    else cout << "Oh My God!";
+    
+    return 0;
+}
